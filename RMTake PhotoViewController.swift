bef5239c99c543b39755c2fad14a12d2e0fc961f
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2023-06-23 01:12:09 +0300

    Add files via upload

---
+++ RMTake PhotoViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RMTake PhotoViewController.swift
+//  your joker
+//
+//  Created by Sumayyah AHmad on 01/12/1444 AH.
+//
+
+import UIKit
+
+class RMTake_PhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+        
+        
+        
+        
+        
+ 
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("ViewDidAppear")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print("ViewWillDisAppear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print("ViewDidDisAppear")
+    }
+    
+}
+
+
